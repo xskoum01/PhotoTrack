f9723d7bdaf3847bc928960a313204ee3ab5d30e
vskoumal <vskoumal@navertica.com>
2024-11-29 20:41:04 +0100

    Merge remote-tracking branch 'origin/main'

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 #konfigurace databaze
 #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.makedirs(os.path.join(basedir, ".gitignore", "instance"), exist_ok=True)}'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, ".gitignore", "instance", "users.db")}' #umisteni databaze
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, ".gitignore", "instance", "users.db")}' #umisteni databaze
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "instance", "users.db")}' #umisteni databaze
 #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "instance", "users.db")}' #umisteni databaze
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
